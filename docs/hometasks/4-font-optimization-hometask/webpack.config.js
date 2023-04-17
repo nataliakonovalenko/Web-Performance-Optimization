5ac933f25a971a547499940b969833443d8f1a40
Natalia Konovalenko <nataly_konovalenko@epam.com>
2023-04-18 00:46:09 +0300

    4 - font optimization

---
+++ docs/hometasks/4-font-optimization-hometask/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, './build'),
+    clean: true,
+  },
+  devServer: {
+    static: path.resolve(__dirname, './build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(s(a|c)ss)$/,
+        use: [MiniCssExtractPlugin.loader,'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(woff|woff2)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({filename: '[name].css'}),
+  ]
+};
