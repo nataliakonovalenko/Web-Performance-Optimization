4ff5125d913579dac4055fb0527a1ffa3c616452
Natalia Konovalenko <nataly_konovalenko@epam.com>
2023-04-18 23:14:27 +0300

    4 - font optimization - added FontPreloadPlugin

--- docs/hometasks/4-font-optimization-hometask/webpack.config.js
+++ docs/hometasks/4-font-optimization-hometask/webpack.config.js
@@ -2,12 +2,14 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const FontPreloadPlugin = require('webpack-font-preload-plugin');
 
 module.exports = {
   mode: 'production',
   entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     path: path.resolve(__dirname, './build'),
+    publicPath: '',
     clean: true,
   },
   devServer: {
@@ -25,7 +27,7 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader,'css-loader', 'sass-loader']
       },
       {
-        test: /\.(woff|woff2)$/i,
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
       },
     ],
@@ -36,5 +38,8 @@ module.exports = {
       filename: 'index.html',
     }),
     new MiniCssExtractPlugin({filename: '[name].css'}),
+    new FontPreloadPlugin({
+      extensions: ["woff", "woff2"],
+    }),
   ]
 };
